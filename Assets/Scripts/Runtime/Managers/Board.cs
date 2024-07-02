48f55d5890bc80f1a305246a37529e5b9efc6a50
Cihan <bulutyusufcihan@gmail.com>
2024-07-02 21:40:06 +0300

    board add

--- Assets/Scripts/Runtime/Managers/Board.cs
+++ Assets/Scripts/Runtime/Managers/Board.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Project Dependency")]
+    [SerializeField] Tile tilePrefab;
+
+    [Header("Scene Dependency")]
+    [SerializeField] Transform tileParent;
+
+    public Tile[] Tiles { get; set; }
+
+    void PrepareTiles()
     {
-        
+        var tileCount = 5;
+        Tiles = new Tile[tileCount];
+        for (int i = 0; i < tileCount; i++)
+        {
+            Tiles[i] = Instantiate(tilePrefab, tileParent);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
