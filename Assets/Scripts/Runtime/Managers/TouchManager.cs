de28cb47983fb23cf6f31294278fb0db13fbbb35
Cihan <bulutyusufcihan@gmail.com>
2024-07-02 21:49:09 +0300

    update TouchManager
    events and interface added

--- Assets/Scripts/Runtime/Managers/TouchManager.cs
+++ Assets/Scripts/Runtime/Managers/TouchManager.cs
@@ -1,18 +1,69 @@
+
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.DeviceSimulation;
 using UnityEngine;
 
 public class TouchManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Camera cam;
+    [SerializeField] string collisionTag;
+    bool _canTouch;
+
     void Start()
     {
-        
+        _canTouch = false;
+        StartCoroutine(WaitForTouch_Cor());
     }
 
-    // Update is called once per frame
+    IEnumerator WaitForTouch_Cor()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        _canTouch = true;
+    }
+
     void Update()
     {
-        
+        if (_canTouch)
+        {
+            GetTouch(Input.mousePosition);
+        }
     }
+
+    private void GetTouch(Vector3 pos)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hit = Physics2D.OverlapPoint(cam.ScreenToWorldPoint(pos));
+            if (CanTouch(hit))
+            {
+                //var selectedCard = hit.gameObject.GetComponent<Card>();
+                //Debug.Log(selectedCard.gameObject.name + " AD");
+                //TouchEvents.OnCardTapped?.Invoke(selectedCard);
+            }
+            else
+            {
+                // TouchEvents.OnEmptyTapped?.Invoke();
+            }
+        }
+    }
+
+    private bool CanTouch(Collider2D hit)
+    {
+        return hit != null && hit.CompareTag(collisionTag);
+    }
+
+
+    public static class TouchEvents
+    {
+        public static Action<ITouchable> OnElementTapped;
+        public static Action OnEmptyTapped;
+    }
+
+    public interface ITouchable
+    {
+
+    }
+
 }
