62909444b71c3e32bf360b89085b4fc692680521
Cihan <bulutyusufcihan@gmail.com>
2024-07-03 22:00:37 +0300

    read level files

--- Assets/Scripts/Runtime/Managers/LevelManager.cs
+++ Assets/Scripts/Runtime/Managers/LevelManager.cs
@@ -3,4 +3,20 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] TextAsset[] levelFiles;
+    LevelData[] _levelData;
+
+    void Awake()
+    {
+        ReadLevels();
+    }
+
+    void ReadLevels()
+    {
+        _levelData = new LevelData[levelFiles.Length];
+
+        for (int i = 0; i < levelFiles.Length; i++)
+        {
+            _levelData[i] = JsonUtility.FromJson<LevelData>(levelFiles[i].text);
+        }
+    }
 }
