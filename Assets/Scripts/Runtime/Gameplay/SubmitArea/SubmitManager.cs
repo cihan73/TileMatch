17db93571fcbf4ab1575f54daf7e70bb86e78a45
Cihan <bulutyusufcihan@gmail.com>
2024-07-24 20:20:35 +0300

    submit manager updated
    word attach/remove/submit callbacks added

--- Assets/Scripts/Runtime/Gameplay/SubmitArea/SubmitManager.cs
+++ Assets/Scripts/Runtime/Gameplay/SubmitArea/SubmitManager.cs
@@ -1,3 +1,5 @@
+
+using System;
 using System.Linq;
 using UnityEngine;
 
@@ -5,6 +7,18 @@ public class SubmitManager : MonoBehaviour
 {
     [SerializeField] SubmitBlock[] submitBlocks;
     public SubmitBlock[] SubmitBlocks => submitBlocks;
+
+    private void Awake()
+    {
+        GameEvents.OnTileAttached += OnWordAttachedCallback;
+        GameEvents.OnTileRemoved += OnWordRemovedCallback;
+    }
+
+    private void OnDestroy()
+    {
+        GameEvents.OnTileAttached -= OnWordAttachedCallback;
+        GameEvents.OnTileRemoved -= OnWordRemovedCallback;
+    }
 
     public bool HasEmptyBlock()
     {
@@ -14,5 +28,30 @@ public class SubmitManager : MonoBehaviour
     public SubmitBlock GetFirstEmptyBlock()
     {
         return submitBlocks.FirstOrDefault(sb => sb.IsEmpty);
+    }
+
+    private void OnWordAttachedCallback(SubmitBlock submitBlock, string word)
+    {
+        var nonEmptyBlocks = submitBlocks.Where(sb => !sb.IsEmpty);
+        var combinedWord = string.Join("", nonEmptyBlocks.Select(sb => sb.Character));
+        //todo: word manager set current word
+    }
+
+    private void OnWordRemovedCallback(SubmitBlock submitBlock)
+    {
+        var nonEmptyBlocks = submitBlocks.Where(sb => !sb.IsEmpty);
+        var combinedWord = string.Join("", nonEmptyBlocks.Select(sb => sb.Character));
+        //todo: word manager set current word
+    }
+
+    private void OnWordSubmittedCallback()
+    {
+        foreach (var block in submitBlocks)
+        {
+            if (!block.IsEmpty)
+            {
+                //todo: clear
+            }
+        }
     }
 }
